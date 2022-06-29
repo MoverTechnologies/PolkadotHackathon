a0ca75c7dc96e6fd7ce0780a7288a9847bcdb56a
pontagon333 <hironosu.work@gmail.com>
2022-06-29 15:04:53 +0900

    Update vestingDeploy file

--- scripts/deploy-vesting.ts
+++ scripts/deploy-vesting.ts
@@ -1,9 +1,15 @@
-import {ethers} from "hardhat";
+import {ethers, upgrades} from "hardhat";
+import * as VestingCon from "../typechain/Vesting";
 
 async function main() {
-    const tokenAddress = "";
     const VestingContract = await ethers.getContractFactory("Vesting");
-    const vesting = await VestingContract.deploy(tokenAddress);
+    const vesting = (await upgrades.deployProxy(
+        VestingContract,
+        ["0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0"], // contract address of Agreement contract
+        {
+            initializer: "initialize",
+        }
+    )) as VestingCon.Vesting;
     await vesting.deployed();
     console.log("vestingContract deployed to: " + vesting.address);
 }
