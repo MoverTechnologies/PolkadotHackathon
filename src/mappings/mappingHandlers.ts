8fc5a93040f79e7d7e650141419f2a676ebfa43e
masafumimori <m.masafumi@outlook.com>
2022-07-06 19:39:26 +0900

    change event handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,25 +1,60 @@
-import { Transfer } from "../types";
-import { MoonbeamCall, MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam';
+import { Transfer } from '../types';
+import {
+	MoonbeamCall,
+	MoonbeamEvent,
+} from '@subql/contract-processors/dist/moonbeam';
 import { BigNumber } from '@ethersproject/bignumber';
 
-type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; value: BigNumber; };
+type AttestEventArgs = [string, BigNumber] & {
+	moderator: string;
+	agreementId: BigNumber;
+};
 
+// export async function handleAttest(
+// 	event: MoonbeamEvent<AttestEventArgs>
+// ): Promise<void> {
+// 	logger.warn('Calling handleAttest');
+// 	logger.warn('event.args._to : ', event.args._to);
+// 	logger.warn('event.args._tokenId : ', event.args._tokenId);
+// 	logger.warn('event.address : ', event.address);
+// 	logger.warn('event.topics : ', event.topics);
+// 	logger.warn('event.logIndex : ', event.logIndex);
+// 	logger.warn('event.data : ', event.data);
 
-export async function handleERC20Transfer(event: MoonbeamEvent<TransferEventArgs>): Promise<void> {
-    logger.warn('Calling handleERC20Transfer');
+// 	// const transfer = Transfer.create({
+// 	// 	amount: event.args.value.toBigInt(),
+// 	// 	from: event.args.from,
+// 	// 	to: event.args.to,
+// 	// 	contractAddress: event.address,
+// 	// 	blockNumber: BigInt(event.blockNumber),
+// 	// 	id: event.transactionHash,
+// 	// });
 
-    const transfer = Transfer.create({
-        amount: event.args.value.toBigInt(),
-        from: event.args.from,
-        to: event.args.to,
-        contractAddress: event.address,
-        blockNumber: BigInt(event.blockNumber),
-        id: event.transactionHash,
-    });
+// 	// await transfer.save();
+// }
 
-    await transfer.save();
+export async function handleCreateAgreement(
+	event: MoonbeamEvent<AttestEventArgs>
+): Promise<void> {
+	logger.warn('Calling handleCreateAgreement');
+	logger.warn('event.args.moderator : ', event.args.moderator);
+	logger.warn('event.args.agreementId : ', event.args.agreementId);
+	logger.warn('event.address : ', event.address);
+	logger.warn('event.topics : ', event.topics);
+	logger.warn('event.logIndex : ', event.logIndex);
+	logger.warn('event.data : ', event.data);
+
+	// const transfer = Transfer.create({
+	// 	amount: event.args.value.toBigInt(),
+	// 	from: event.args.from,
+	// 	to: event.args.to,
+	// 	contractAddress: event.address,
+	// 	blockNumber: BigInt(event.blockNumber),
+	// 	id: event.transactionHash,
+	// });
+
+	// await transfer.save();
 }
-
 
 // export async function handleEVMCall(call: MoonbeamCall): Promise<void> {
 
